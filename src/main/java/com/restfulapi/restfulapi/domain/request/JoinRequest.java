46422e7e350aecd61d1993095804c7829296c0ab
Domae-back-end <ty_ty123@naver.com>
2023-06-02 12:46:52 +0900

    just: 띄어쓰기 추가...

--- src/main/java/com/restfulapi/restfulapi/domain/request/JoinRequest.java
+++ src/main/java/com/restfulapi/restfulapi/domain/request/JoinRequest.java
@@ -1,9 +1,11 @@
 package com.restfulapi.restfulapi.domain.request;
 
 import jakarta.persistence.Column;
+import jakarta.validation.Valid;
 import lombok.Setter;
 
 public record JoinRequest(
+
         String userId,
         String password,
         String email,
