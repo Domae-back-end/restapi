a8ddc5112e83f48cacdd7c340ff63550511111e7
Domae-back-end <ty_ty123@naver.com>
2023-06-02 12:46:34 +0900

    update: 에러코드 추가

--- src/main/java/com/restfulapi/restfulapi/domain/type/ErrorCode.java
+++ src/main/java/com/restfulapi/restfulapi/domain/type/ErrorCode.java
@@ -7,8 +7,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
-
-    USERID_SAME(HttpStatus.UNAUTHORIZED, "동일한 아이디가 있습니다.")
+    //회원가입
+    USERID_SAME(HttpStatus.UNAUTHORIZED, "동일한 아이디가 있습니다."),
+    EMAIL_TIME_OVER(HttpStatus.UNAUTHORIZED, "이메일 인증 시간 초과"),
+    EMAIL_NOT_SUCCESS_KEY(HttpStatus.UNAUTHORIZED, "이메일 인증이 안됩니다.")
     ;
 
     private final HttpStatus status;
