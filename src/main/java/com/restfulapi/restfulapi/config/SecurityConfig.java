76fb5371972a4b2d71718edf8fbcfa6299c1badf
Domae-back-end <ty_ty123@naver.com>
2023-06-10 21:18:48 +0900

    update: 시큐리티 기능개발을 위해 임시로 모든 주소 허용

--- src/main/java/com/restfulapi/restfulapi/config/SecurityConfig.java
+++ src/main/java/com/restfulapi/restfulapi/config/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.restfulapi.restfulapi.config;
 
+import com.restfulapi.restfulapi.domain.type.ErrorCode;
+import com.restfulapi.restfulapi.exception.ApplicationException;
 import com.restfulapi.restfulapi.service.LoginService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +18,8 @@ public class SecurityConfig {
 
     private final LoginService loginService;
     private final SecurityFailHandler securityFailHandler;
+    private final SecuritySuccessHandler successHandler;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -22,23 +28,27 @@ public class SecurityConfig {
                         .disable()
                 )
                 .authorizeHttpRequests(req -> req
-                        .requestMatchers(
-                                "/api/v1/register",
-                                "/api/v1/email",
-                                "/api/v1/email-success",
-                                "/api/v1/login",
-                                "/api/v1/forget",
-                                "/api/forget/**"
-                        ).permitAll()
-                        .anyRequest().authenticated()
+//                        .requestMatchers(
+//                                "/api/v1/register",
+//                                "/api/v1/email",
+//                                "/api/v1/email-success",
+//                                "/api/v1/login",
+//                                "/api/v1/forget",
+//                                "/api/forget/**"
+//                        ).permitAll()
+//                        .anyRequest().authenticated()
+                                .anyRequest().permitAll()
                 )
                 .formLogin(login -> login
                         .loginProcessingUrl("/api/v1/login")
                         .usernameParameter("userid")
                         .passwordParameter("password")
                         .failureHandler(securityFailHandler)
+                        .successHandler(successHandler)
+
                 )
-                .userDetailsService(loginService);
+                .authenticationProvider(loginService)
+
 
         ;
 
