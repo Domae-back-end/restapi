dc72af199c599c03d1e704ed0967871c4c9877ed
Domae-back-end <ty_ty123@naver.com>
2023-06-12 16:38:35 +0900

    update : 에러 코드 추가

--- src/main/java/com/restfulapi/restfulapi/domain/type/ErrorCode.java
+++ src/main/java/com/restfulapi/restfulapi/domain/type/ErrorCode.java
@@ -17,8 +17,18 @@ public enum ErrorCode {
     USER_NOT_LOGIN(HttpStatus.UNAUTHORIZED,"아이디 또는 비밀번호가 맞지 않습니다."),
 
     //비밀번호 찾기
-    FORGET_NO_ADDRESS(HttpStatus.BAD_REQUEST, "잘못된 주소 접근입니다.");
+    FORGET_NO_ADDRESS(HttpStatus.BAD_REQUEST, "잘못된 주소 접근입니다."),
 
+    //SHop
+    NO_SHOP_PID(HttpStatus.BAD_REQUEST, "잘못된 상품 접근입니다."),
+
+    //Food
+    NO_FOOD_PID(HttpStatus.BAD_REQUEST,"잘못된 음식 접근입니다."),
+
+    //file
+    NO_EXTENSION(HttpStatus.BAD_REQUEST,"잘못된 파일 확장자 입니다."),
+    ERROR_FILE(HttpStatus.INTERNAL_SERVER_ERROR, "파일 삭제 도중 오류 발생."),
+    NO_FILE(HttpStatus.INTERNAL_SERVER_ERROR, "파일이 존재하지 않아 오류 발생")
     ;
 
     private final HttpStatus status;
